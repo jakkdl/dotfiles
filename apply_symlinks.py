f1fba8bf9ab3cefd2b46eb387c6a2b450e78ffc2
jakkdl <h6+github@pm.me>
2022-02-08 15:34:31 +0100

    changed structure, wrote script to automate symlinking, made vim-plug a submodule

---
+++ apply_symlinks.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import os
+import os.path
+
+def main():
+    handle_folder('home', os.path.expanduser('~'), '.')
+    handle_folder('root', os.path.expanduser('/'))
+
+def handle_folder(folder, replace, prefix=''):
+    #homedir = os.path.expanduser('~')
+    #homedir = replace
+    for dirpath, dirnames, filenames in os.walk(folder):
+        if dirpath == folder:
+            base = os.path.join(replace, prefix)
+            basepath = replace
+        else:
+            base = os.path.join(replace, prefix + dirpath[len(folder)+1:], '')
+            basepath = base
+
+
+        for filename in filenames:
+            #print(base + filename, os.path.join(dirpath, filename))
+            path = base+filename
+            real_target_path = os.path.realpath(os.path.join(dirpath, filename))
+            target = os.path.relpath(real_target_path, basepath)
+            if not (os.path.isfile(path) or os.path.islink(path)):
+                print(f'{path} not a file')
+                fix(path, target)
+                continue
+            elif not os.path.islink(path):
+                print(f'{path} not a symlink')
+                continue
+            actual_target = os.readlink(path)
+            if actual_target != target and actual_target != real_target_path:
+                print(f'{path} incorrect target\n\t{actual_target} '
+                        f'should be\n\t{target}')
+                fix(path, target)
+
+def fix(path, target):
+    if 'home' not in path:
+        print(f'run: sudo ln -s {os.path.realpath(target)} {path}')
+        return
+    if input(f'\tresolve? [Y/n]: ').strip() != 'n':
+        if os.path.islink(path):
+            os.remove(path)
+        os.symlink(target, path)
+
+if __name__ == '__main__':
+    print(__file__)
+    main()
+
