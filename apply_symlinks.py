fe1d6bd713021ddb3a4a0e508760ff90f2e41dc2
jakkdl <h6+github@pm.me>
2022-03-21 09:26:54 +0100

    getting apply_symlinks to print if no errors were encountered, and post-commit to call it

--- apply_symlinks.py
+++ apply_symlinks.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+"""Checks that all files in the root and home directories have symlinks on the system,
+and prompts to create or warns about missing them"""
 import os
 import os.path
 
 def main():
-    handle_folder('home', os.path.expanduser('~'), '.')
-    handle_folder('root', os.path.expanduser('/'))
+    """do all the stuffs"""
+    if not (
+            handle_folder('home', os.path.expanduser('~'), '.') or
+            handle_folder('root', os.path.expanduser('/')) or
+            check_submodules()
+            ):
+        print("all symlinks OK")
+
+def check_submodules():
+    """check for submodules"""
     if not os.path.isfile('vim-plug/plug.vim'):
         print('run submodule init && submodule update')
+        return True
+    return False
 
 def handle_folder(folder, replace, prefix=''):
+    """returns the number of uncorrected errors"""
+    assert folder in ('home', 'root')
+    errors = 0
     #homedir = os.path.expanduser('~')
     #homedir = replace
-    for dirpath, dirnames, filenames in os.walk(folder):
+    for dirpath, _dirnames, filenames in os.walk(folder):
         if dirpath == folder:
             base = os.path.join(replace, prefix)
             basepath = replace
@@ -25,39 +40,39 @@ def handle_folder(folder, replace, prefix=''):
             path = base+filename
             real_target_path = os.path.realpath(os.path.join(dirpath, filename))
             target = os.path.relpath(real_target_path, basepath)
+            pref_target = real_target_path if folder == 'root' else target
+
             if not (os.path.isfile(path) or os.path.islink(path)):
                 print(f'{path} not a file')
-                if folder == 'home':
-                    fix(path, target)
-                elif folder == 'root':
-                    fix(path, real_target_path)
-                continue
+                errors += fix(path, pref_target)
             elif not os.path.islink(path):
                 print(f'{path} not a symlink')
+                errors += 1
                 continue
+
             actual_target = os.readlink(path)
-            if actual_target != target and actual_target != real_target_path:
+            if actual_target not in (target, real_target_path):
                 print(f'{path} incorrect target\n\t{actual_target} '
-                        f'should be\n\t{target}')
-                if folder == 'home':
-                    fix(path, target)
-                elif folder == 'root':
-                    fix(path, real_target_path)
+                        f'should be\n\t{pref_target}')
+                errors += fix(path, pref_target)
+    return errors
 
 def fix(path, target):
+    """fix, or suggest how to fix, an incorrect symlink"""
     if 'home' not in path:
         print(target)
         print(f'run: sudo ln -s {os.path.realpath(target)} {path}')
-        return
-    if input(f'\tresolve? [Y/n]: ').strip() != 'n':
+        return 1
+    if input("\tresolve? [Y/n]: ").strip() != "n":
         if os.path.islink(path):
             os.remove(path)
         if not os.path.isdir(os.path.dirname(path)):
             print(f'creating directory {os.path.dirname(path)}')
             os.makedirs(os.path.dirname(path))
         os.symlink(target, path)
+        return 0
+    return 1
 
 if __name__ == '__main__':
     print(__file__)
     main()
-
