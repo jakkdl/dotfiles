b028858e895e7eca4f10c519819d34b20239e7d2
jakkdl <h6+github@pm.me>
2022-03-22 22:12:05 +0000

    more work on apply_symlinks, remove arrows from herbstluftwm so as not to collide with discord

--- apply_symlinks.py
+++ apply_symlinks.py
@@ -6,24 +6,26 @@ import os.path
 
 def main(interactive = True):
     """do all the stuffs"""
-    if any([
-            handle_folder('home', os.path.expanduser('~'), interactive, '.'),
-            handle_folder('root', os.path.expanduser('/'), interactive),
-            check_submodules()
-            ]):
+    commands = handle_folder('root', os.path.expanduser('/'), interactive)
+    commands += handle_folder('home', os.path.expanduser('~'), interactive, '.')
+    commands.append(check_submodules())
+    if not commands:
         print("all symlinks OK")
+        return
+
+    with open("fix_links.sh", "w") as file:
+        file.write('\n'.join(commands))
 
 def check_submodules():
     """check for submodules"""
     if not os.path.isfile('vim-plug/plug.vim'):
-        print('run submodule init && submodule update')
-        return True
-    return False
+        return "git submodule init && git submodule update\n"
+    return ""
 
 def handle_folder(folder, replace, interactive, prefix=''):
-    """returns the number of uncorrected errors"""
+    """returns a list of commands needed to fix all errors"""
     assert folder in ('home', 'root')
-    errors = 0
+    commands = []
     #homedir = os.path.expanduser('~')
     #homedir = replace
     for dirpath, _dirnames, filenames in os.walk(folder):
@@ -42,32 +44,38 @@ def handle_folder(folder, replace, interactive, prefix=''):
             pref_target = real_target_path if folder == 'root' else target
 
             if folder == 'root' and os.access(real_target_path, os.W_OK):
-                print(f"WARNING: dangerous write access, execute:\n"
-                        f"sudo chown root:root {real_target_path}")
+                print(f"WARNING: dangerous write access {os.path.join(dirpath+path)}")
+                commands.append(f"sudo chown root:root {real_target_path}")
+
             if not (os.path.isfile(path) or os.path.islink(path)):
-                print(f'{path} not a file')
-                errors += fix(path, pref_target, interactive)
+                print(f'{path} missing')
+                commands.append(fix(path, pref_target, interactive))
                 continue
-            elif not os.path.islink(path):
-                print(f'{path} not a symlink')
-                errors += 1
+
+            if not os.path.islink(path):
+                print(f'{path} not a symlink, inspect with diff')
                 continue
 
             actual_target = os.readlink(path)
             if actual_target not in (target, real_target_path):
                 print(f'{path} incorrect target\n\t{actual_target} '
                         f'should be\n\t{pref_target}')
-                errors += fix(path, pref_target, interactive)
-    return errors
+                commands.append(fix(path, pref_target, interactive))
+    return commands
 
 def fix(path, target, interactive):
     """fix, or suggest how to fix, an incorrect symlink"""
     if 'home' not in path:
-        print(target)
-        print(f'run: sudo ln -s {os.path.realpath(target)} {path}')
-        return 1
+        # print(target)
+        return f'sudo ln -s {os.path.realpath(target)} {path}'
+
     if not interactive:
-        return 1
+        if not os.path.isdir(os.path.dirname(path)):
+            res = f"mkdir {os.path.dirname(path)}\n"
+        else:
+            res = ""
+        return res + f"ln -s {target} {path}"
+
     if "n" not in input("\tresolve? [Y/n]: "):
         if os.path.islink(path):
             os.remove(path)
@@ -75,9 +83,8 @@ def fix(path, target, interactive):
             print(f'creating directory {os.path.dirname(path)}')
             os.makedirs(os.path.dirname(path))
         os.symlink(target, path)
-        return 0
-    return 1
+        return ""
 
 if __name__ == '__main__':
-    print(__file__)
+    #print(__file__)
     main()
