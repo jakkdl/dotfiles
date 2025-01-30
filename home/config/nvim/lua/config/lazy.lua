2adc9d6d046e8cd05d405f6574a3544915b24098
jakkdl <h6+github@pm.me>
2025-01-30 10:28:47 +0100

    only check for lazy plugin updates weekly

--- home/config/nvim/lua/config/lazy.lua
+++ home/config/nvim/lua/config/lazy.lua
@@ -31,5 +31,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "gruvbox" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+      enabled = true,
+      frequency = 7*24*3600
+  },
 })
